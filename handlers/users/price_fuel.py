d228e46bbd29fbf9b061aed816bd8e790d57a605
Maks <maks.karalash@gmail.com>
2022-03-29 23:46:23 +0300

    chromedriver

--- handlers/users/price_fuel.py
+++ handlers/users/price_fuel.py
@@ -18,11 +18,11 @@ async def price_fuel(message: types.Message):
 
     options = webdriver.ChromeOptions()
     options.set_capability('general.useragent.override',
-                           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36')
+                           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
 
 
     driver = webdriver.Chrome(
-        executable_path="geckodriver",
+        executable_path="chromedriver",
         options=options
     )
     driver.get(url)
